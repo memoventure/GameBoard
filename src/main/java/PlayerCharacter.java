9591f4fcec2a4f215ca455f3414cc755e1258be0
anna-koenig <anna-koenig@web.de>
2025-01-30 13:02:29 +0100

    add advanced tests and player class

--- src/main/java/PlayerCharacter.java
+++ src/main/java/PlayerCharacter.java
@@ -1,22 +1,49 @@
 public class PlayerCharacter {
 
+    static int posX = 0;
+    static int posY = 0;
+
     public static int getX()
     {
-        return 0;
+        return posX;
     }
 
     public static int getY()
     {
-        return 0;
+        return posY;
     }
 
-    public static int move(String keyboardLetter) {
-        if(keyboardLetter.equals("W")){
-            return 1;
-        }
-        else
+    public static int setX(int x)
+    {
+        posX = x;
+        return posX;
+    }
+
+    public static int setY(int y)
+    {
+        posY = y;
+        return posY;
+    }
+
+    public static void move(String keyboardLetter) {
+        switch(keyboardLetter)
         {
-            return 0;
+            case("S"): // go up
+                posY+=-1;
+                break;
+            case("A"): // go left
+                posX+=-1;
+                break;
+            case("W"): // go down
+                posY+=1;
+                break;
+            case("D"): //go right
+                posX+=1;
+                break;
+            default:
+                System.out.println("No valid movement");
+                break;
         }
+        return;
     }
 }
--- src/test/java/TestGameBoard.java
+++ src/test/java/TestGameBoard.java
@@ -1,10 +1,17 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 
 public class TestGameBoard {
+    @BeforeEach
+    void initialize()
+    {
+        PlayerCharacter.posX = 0;
+        PlayerCharacter.posY = 0;
+    }
 
     @Test
-    void expect0_whenCalledGetX()
+    void expect0_whenCalledGetXAtBeginning()
     {
         // GIVEN
         // not needed;
@@ -16,27 +23,108 @@ public class TestGameBoard {
     }
 
     @Test
-    void expect0_whenCalledGetY()
+    void expectX_whenCalledGetX()
+    {
+        // GIVEN
+        // not needed;
+        // WHEN
+        int actual = PlayerCharacter.getX();
+        // THEN
+        int expected = PlayerCharacter.posX;
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void expectY_whenCalledGetY()
     {
         // GIVEN
         // not needed;
         // WHEN
         int actual = PlayerCharacter.getY();
         // THEN
-        int expected = 0;
+        int expected = PlayerCharacter.posY;;
         Assertions.assertEquals(expected, actual);
     }
 
     @Test
-    void expectIncrementY_whenCalledWithW()
+    void expectY3_whenCalledWithSAndY2()
+    {
+        // GIVEN
+        String keyboardLetter = "S";
+        PlayerCharacter.setY(2);
+        // WHEN
+        PlayerCharacter.move(keyboardLetter);
+        // THEN
+        int expected = 1;
+        Assertions.assertEquals(expected, PlayerCharacter.posY);
+    }
+
+    @Test
+    void expectDecrementY_whenCalledWithW()
+    {
+        // GIVEN
+        String keyboardLetter = "S";
+        int currentPosY = PlayerCharacter.posY;
+        // WHEN
+        PlayerCharacter.move(keyboardLetter);
+        // THEN
+        int expected = currentPosY - 1;
+        Assertions.assertEquals(expected, PlayerCharacter.posY);
+    }
+
+    @Test
+    void expectInccrementY_whenCalledWithS()
     {
         // GIVEN
         String keyboardLetter = "W";
-
+        int currentPosY = PlayerCharacter.posY;
         // WHEN
-        int actual = PlayerCharacter.move(keyboardLetter);
+        PlayerCharacter.move(keyboardLetter);
         // THEN
-        int expected = 1;
-        Assertions.assertEquals(expected, actual);
+        int expected = currentPosY + 1;
+        Assertions.assertEquals(expected, PlayerCharacter.posY);
+    }
+
+    @Test
+    void expectDecrementX_whenCalledWithA()
+    {
+        // GIVEN
+        String keyboardLetter = "A";
+        int currentPosX = PlayerCharacter.posX;
+        // WHEN
+        PlayerCharacter.move(keyboardLetter);
+        // THEN
+        int expected = currentPosX - 1;
+        Assertions.assertEquals(expected, PlayerCharacter.posX);
+    }
+
+    @Test
+    void expectIncrementX_whenCalledWithD()
+    {
+        // GIVEN
+        String keyboardLetter = "D";
+        int currentPosX = PlayerCharacter.posX;
+        // WHEN
+        PlayerCharacter.move(keyboardLetter);
+        // THEN
+        int expected = currentPosX + 1;
+        Assertions.assertEquals(expected, PlayerCharacter.posX);
+    }
+
+    @Test
+    void expectX1Y1_whenCalledWithRRWA()
+    {
+        // GIVEN
+        String[] myLetters = {"D", "D", "W", "A"};
+        // WHEN
+        for (String currLetter : myLetters)
+        {
+            PlayerCharacter.move(currLetter);
+        }
+        // THEN
+        int expectedY = 1;
+        int expectedX = 1;
+        Assertions.assertEquals(expectedX, PlayerCharacter.posX);
+        Assertions.assertEquals(expectedY, PlayerCharacter.posY);
     }
 }
